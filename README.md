e99d502fad13f060beab96b913f1ae7c183f085f
James Harrison <James.Harrison.za@gmail.com>
2018-05-04 16:46:21 +0200

    WIP

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ExpressWebserver
 Node webserver with Express
+
+## Docker commands
+* docker pull mongo
+* docker run --rm --name mongodb -v ExpressWebServer:/data/db -d -p 27017:27017 mongo
+* docker run --rm --name mongodb -v D:/_James\MyCode/ExpressWebserver/data:/data/db -it -p 27017:27017 mongo
+
+### Create Admin user
+* docker exec -it mongodb mongo admin
+* db.createUser({ user: 'james', pwd: '42', roles: [ { role: "userAdminAnyDatabase", db: "admin" } ] });
+
+### Insert Data
+* docker exec -it mongodb mongo admin
+* 
--- src/server.js
+++ src/server.js
@@ -1,12 +1,58 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
+const dbName = 'bookAPI';
+const collectionName = 'books';
+const url = `mongodb://localhost:27017/${dbName}`;
+
+// ----------------------------------------------------------------------------------------------------------------------------------
+// Creating some records
+MongoClient.connect(url, function (err, db) {
+	if (err) throw err;
+
+	const dbo = db.db(dbName);
+
+	dbo.createCollection(collectionName, function (err, res) {
+		if (err) throw err;
+		console.log("Collection created!");
+		db.close();
+	});
+
+	var myobj = {
+		title: 'War and Peace',
+		genre: 'Historical Fiction',
+		author: 'Lev Nikolayevich Tolstoy',
+		read: false
+	};
+	dbo.collection(collectionName).insertOne(myobj, function (err, res) {
+		if (err) throw err;
+		console.log("1 document inserted");
+		db.close();
+	});
+});
+// ----------------------------------------------------------------------------------------------------------------------------------
 
 const app = express();
 const port = process.env.PORT || 42420;
+const router = express.Router();
 
 app.get('/', (req, res) => {
 	res.send('welcome to my API!');
 });
 
+router.route('/Books')
+	.get(async (req, res) => {
+
+		const mongoClient = await MongoClient.connect(url);
+		const db = mongoClient.db(dbName);
+		const results = await db.collection(collectionName).find({}).toArray();
+		mongoClient.close();
+		res.json(results);
+	});
+
+app.use('/api', router);
+
 app.listen(port, () => {
-	console.log(`Listening on http://localhost:${port}`)
+	console.log(`Listening on http://localhost:${port}`);
+	console.log(`API running at http://localhost:${port}/api/books`);
 });
